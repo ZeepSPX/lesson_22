3e90b870c331164cc9d485cd1ce4154c80cb4d18
ZeepSPX <100509840+ZeepSPX@users.noreply.github.com>
2022-11-25 16:15:15 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,74 @@
+"use strict"
+
+// 1
+console.log(`Вчимо JS`)
+
+// 2
+console.log(`JS`)
+console.log(`Вчимо`)
+
+// 3
+console.log(`Вчимо`); console.log(`JS`);
+
+// 4
+console.log(`JS`);
+console.log(`Вчимо`);
+
+// ==========================================================================
+
+const COLOR_BROWN = `#8B4513`;
+const COLOR_GREEN = `#008000`;
+const COLOR_BLUE = `#4682B4`;
+const COLOR_GRAY = `#A9A9A9`;
+
+let userColorEyes;
+
+userColorEyes = COLOR_BLUE;
+console.log(userColorEyes);
+
+// let user;
+// let userName = `Василь`;
+
+// user = userName;
+// console.log(user);
+
+// let user = `Василь`, let age = 36;
+// let user = `Василь`, age = 36;
+
+// let myAge = 36;
+
+// const BLOCK_SIZE = 14 + 50;
+// console.log(BLOCK_SIZE);
+
+// if (true) {
+// 	var size = 15;
+// }
+// console.log(size);
+
+// ===================================================================================
+
+
+let userAge = 36;
+let userInfo = `Фрілансер ${userAge}`;
+
+console.log(userInfo);
+console.log(typeof userInfo);
+
+console.log(userAge);
+console.log(typeof userAge);
+
+let userHeight = 145 / 0;
+
+console.log(userHeight);
+console.log(typeof userHeight);
+
+let userName;
+
+console.log(userName);
+console.log(typeof userName);
+
+let userSize = "45" / "8";
+
+
+console.log(userSize);
+console.log(typeof userSize);
